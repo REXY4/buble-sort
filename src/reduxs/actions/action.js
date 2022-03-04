6c8c259e5cc7dcdc29bd271a7098c67354c8ab26
REXY4 <muhammadrzki96@gmail.com>
2022-03-04 16:38:20 +0700

    first commit

--- src/reduxs/actions/action.js
+++ src/reduxs/actions/action.js
@@ -1,10 +1,30 @@
 export const BubleSortAction = (value, route, dispatch) => {
-    let array = value;
- let buble = array.sort((i, j) => { return i - j })
-  route.push("/pagetwo")
+    var count = 0
+    var length = array.length
+  
+    // traverse the array (length of array) times
+    while (count !== length) {
+      for (var i = length; i > count; i--) {
+        var current = array[i]
+        var left = array[i - 1]
+  
+        // delete element if value is null
+        if (current === null) {
+          array.splice(i, 1)
+          continue
+        }
+  
+        // swap
+        if (left !== undefined && left > current) {
+          array[i - 1] = current
+          array[i] = left
+        }
+      }
+      count++
+    }
   return dispatch({
       type : "ADD_DATA",
-      payload : buble
+      payload : array
   })
 } 
 
