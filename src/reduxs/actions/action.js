e47ab55b600a6a6743e9bbdd5a848c2ef35c25d7
REXY4 <muhammadrzki96@gmail.com>
2022-03-04 16:38:52 +0700

    first commit

--- src/reduxs/actions/action.js
+++ src/reduxs/actions/action.js
@@ -2,19 +2,17 @@ export const BubleSortAction = (value, route, dispatch) => {
     var count = 0
     var length = array.length
   
-    // traverse the array (length of array) times
     while (count !== length) {
       for (var i = length; i > count; i--) {
         var current = array[i]
         var left = array[i - 1]
   
-        // delete element if value is null
+       
         if (current === null) {
           array.splice(i, 1)
           continue
         }
   
-        // swap
         if (left !== undefined && left > current) {
           array[i - 1] = current
           array[i] = left
